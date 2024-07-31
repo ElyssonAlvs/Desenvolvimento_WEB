24b1c808b8d1b72201fe99a24afc88cf610ce0c2
Elysson Alves <elysson250102@gmail.com>
2024-07-31 16:27:51 -0300

    Introdução

--- WEB/Introducao/src/App.js
+++ WEB/Introducao/src/App.js
@@ -1,27 +1,10 @@
-// import HelloWorld from "./components/01-Hello_World/HelloWorld";
-// import FuncoesInternas from "./components/02-Funcoes_internar/FuncoesInternas";
-// import Loja from "./components/03-Props/Loja";
-// import Curso from "./components/03-Props/Curso"
-// import MeusDadosProps from "./components/05-Atividade00/02MeusDados"
-// import Informacao from "./components/05-Atividade00/informacao";
-// import Dados from "./components/05-Atividade00/01MeusDados";
-// import Temperatura from "./components/05-Atividade00/03Temperatura"
-// import SistemaSolar from "./components/06-Children/SistemaSolar";
-// import Planeta from "./components/06-Children/Planeta";
-// import Galaxia from "./components/06-Children/Galaxia";
-// import ComPai from "./components/07-ComPaiFilho/ComPai";
-// import Estado from "./components/08-Estados/Estado";
-// import Pokemon from "./components/08-Estados/Pokemon";
-// import { CompA } from "./components/09-PropDrilling/PropDrilling";
-// import PokemonPrincipal from "./components/11-PokemonContexto/PokemonPrincipal";
-// import Efeito from "./components/11-Efeitos/Efeito";
-import ComponentePromessa from "./components/12-Promessa/MeuAsync";
+import Main from "./components/18-CRUD/Main";
 
 function App() {
   return (
     <div>
       <h1>Desenvolvimento de Software para a WEB!</h1>
-      <ComponentePromessa></ComponentePromessa>
+      <Main/>
     </div>
   )
 }
