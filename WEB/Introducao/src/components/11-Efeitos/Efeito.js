bd4b1e30d9a5f676d01f8fc56a8cfd507d138276
Elysson Alves <elysson250102@gmail.com>
2024-08-20 22:16:37 -0300

    Efeitos

--- WEB/Introducao/src/components/11-Efeitos/Efeito.js
+++ WEB/Introducao/src/components/11-Efeitos/Efeito.js
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react";
 
+// Componente funcional Efeito
 const Efeito = () => {
+    // Declara dois estados: 'contador' e 'ehPar'
+    // 'contador' é inicializado com 0 e 'ehPar' com true
     const [contador, setContador] = useState(0);
     const [ehPar, setEhPar] = useState(true);
 
+    // useEffect é usado para realizar efeitos colaterais, neste caso,
+    // para verificar se o contador é par ou ímpar toda vez que 'contador' mudar.
     useEffect(() => {
+        // Verifica se 'contador' é par
         if (contador % 2 === 0) setEhPar(true);
+        // Caso contrário, é ímpar
         else setEhPar(false);
-    }, [contador]);
+    }, [contador]); // 'contador' é a dependência, o efeito é executado toda vez que 'contador' mudar
 
+    // Renderiza o componente
     return (
         <div>
+            {/* Exibe o valor atual de 'contador' */}
             <h2>Contador : {contador}</h2>
+            {/* Exibe se 'contador' é par ou ímpar */}
             <h3> {contador} é par ? {ehPar + ""}</h3>
+            {/* Botão que incrementa 'contador' ao ser clicado */}
             <button onClick={() => setContador((Anterior) => contador + 1)}>
                 Aumentar!
             </button>
@@ -20,4 +31,5 @@ const Efeito = () => {
     );
 };
 
+// Exporta o componente para ser usado em outras partes da aplicação
 export default Efeito;
