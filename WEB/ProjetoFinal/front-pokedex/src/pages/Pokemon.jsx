614075bdba840a7496be968da0c4020c57466b11
Elysson Alves <elysson250102@gmail.com>
2024-09-24 14:55:20 -0300

    Aplicando os filtros

--- WEB/ProjetoFinal/front-pokedex/src/pages/Pokemon.jsx
+++ WEB/ProjetoFinal/front-pokedex/src/pages/Pokemon.jsx
@@ -6,17 +6,23 @@ import PokemonCard from "../components/PokemonCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+
 const Pokemon = () => {
 
     const [pokemons, setPokemons] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [typeFilter, setTypeFilter] = useState("");
+    const [sortBy, setSortBy] = useState("");
     const [filteredPokemons, setFilteredPokemons] = useState([]);
 
     useEffect(() => {
         getPokemons();
     }, []);
 
+
     useEffect(() => {
+        let filtered = pokemons;
+
         // Filtra os pokémons conforme o termo de busca muda
         if (searchTerm === "") {
             setFilteredPokemons(pokemons); // Exibe todos se a busca estiver vazia
@@ -26,7 +32,24 @@ const Pokemon = () => {
             );
             setFilteredPokemons(filtered);
         }
-    }, [searchTerm, pokemons]);
+
+        if (sortBy === "name") {
+            filtered = [...filtered].sort((a, b) => a.name.localeCompare(b.name));
+        } else if (sortBy === "life") {
+            filtered = [...filtered].sort((a, b) => b.stats[0].base_stat - a.stats[0].base_stat);
+        } else if (sortBy === "attack") {
+            filtered = [...filtered].sort((a, b) => b.stats[1].base_stat - a.stats[1].base_stat);
+        } else if (sortBy === "defense") {
+            filtered = [...filtered].sort((a, b) => b.stats[2].base_stat - a.stats[2].base_stat);
+        } else if (sortBy === "speed") {
+            filtered = [...filtered].sort((a, b) => b.stats[5].base_stat - a.stats[5].base_stat);
+        } else if (sortBy === "id") {
+            filtered = [...filtered].sort((a, b) => a.id - b.id); // Reseta para o ID
+        }
+
+        // Atualiza o estado com a lista filtrada
+        setFilteredPokemons(filtered);
+    }, [searchTerm, pokemons, typeFilter, sortBy]);
 
     const getPokemons = () => {
         const endpoints = [];
@@ -153,6 +176,7 @@ const Pokemon = () => {
                         marginRight: "320px",
                     }}
                 >
+                    {/* Campo de busca */}
                     <input
                         type="text"
                         className="form-control"
@@ -161,6 +185,90 @@ const Pokemon = () => {
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                     />
+
+                    {/* Botões de Filtros */}
+                    <div className="d-flex justify-content-center mb-3">
+                        <div className="btn-group" role="group" aria-label="Filtros">
+                            <button
+                                type="button"
+                                className={`btn btn-secondary ${sortBy === "id" ? "active" : ""}`}
+                                onClick={() => setSortBy("id")}
+                            >
+                                ID
+                            </button>
+                            <button
+                                type="button"
+                                className={`btn btn-secondary ${sortBy === "nome" ? "active" : ""}`}
+                                onClick={() => setSortBy("name")}
+                            >
+                                Nome
+                            </button>
+                            {/* Dropdown para Tipo */}
+                            <div className="btn-group ms-2" role="group">
+                                <button
+                                    id="btnGroupDrop1"
+                                    type="button"
+                                    className="btn btn-secondary dropdown-toggle"
+                                    data-bs-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
+                                >
+                                    Tipo: {typeFilter}
+                                </button>
+                                <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => setTypeFilter("Todos")}
+                                    >
+                                        Todos
+                                    </a>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => setTypeFilter("Normal")}
+                                    >
+                                        Normal
+                                    </a>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => setTypeFilter("Fogo")}
+                                    >
+                                        Fogo
+                                    </a>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => setTypeFilter("Água")}
+                                    >
+                                        Água
+                                    </a>
+                                </div>
+                            </div>
+                            <button
+                                type="button"
+                                className={`btn btn-secondary ${sortBy === "life" ? "active" : ""}`}
+                                onClick={() => setSortBy("life")}
+                            >
+                                Vida
+                            </button>
+                            <button
+                                type="button"
+                                className={`btn btn-secondary ${sortBy === "attack" ? "active" : ""}`}
+                                onClick={() => setSortBy("attack")}
+                            >
+                                Ataque
+                            </button>
+                            <button
+                                type="button"
+                                className={`btn btn-secondary ${sortBy === "speed" ? "active" : ""}`}
+                                onClick={() => setSortBy("speed")}
+                            >
+                                Velocidade
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 <div className="container">
                     <div className="row">
